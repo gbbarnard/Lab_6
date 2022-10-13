1012774f49b6426b73d579976d4dfca677a15b0a
Gavin Barnard <gbbarnard@oakland.edu>
2022-10-13 14:42:50 -0400

    second commit

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,21 +1,18 @@
-import java.util.*;
-
 public class Fibonacci {
-    public static void main(String args[]) {
+    public static void main(String args[]){
         int n;
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-        System.out.print("Please enter the Fibonacci number: ");
-        n = scanner.nextInt();
-        
-        System.out.println("The answer is: " + fib(n));
 
-        System.out.println("Would you like to continue? Please enter yes or no");
-        String YesNo = scanner.next();
-        if (YesNo.equals("no")) break;
+        if (args.length > 0) {
+            try {
+                n = Integer.parseInt(args[0]);
+                System.out.println(fib(n));
+            } catch (NumberFormatException e) {
+                System.err.println("Argument" + args[0] + " must be an integer.");
+                System.exit(1);
+            }
         }
-        scanner.close();
     }
+
     public static int fib(int n) {
         if (n == 0) {
             return 0;
